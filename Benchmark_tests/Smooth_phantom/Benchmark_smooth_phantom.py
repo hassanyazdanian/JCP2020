d5f8ebe59e67eb2713cf5d33036dbcfdd0008517
hasyaz <hassanyazdanian@gmail.com>
2022-06-05 06:37:34 +0200

    Update Benchmark_smooth_phantom.py

--- Benchmark_tests/Smooth_phantom/Benchmark_smooth_phantom.py
+++ Benchmark_tests/Smooth_phantom/Benchmark_smooth_phantom.py
@@ -47,15 +47,15 @@ def GRID(s, m):
     M = 2**m		# number of grid points in each direction. we choose M as a power of 2 to speed up fft computation.
     h = s*2./M   	# step size
     x = np.linspace((-2**(m-1)+1./2)*h,(2**(m-1)-1./2)*h,M)	 # grid points in each direction
-    X, Y, Z = np.meshgrid(x, x, x, indexing='ij', sparse = True)	 # Evaluation cells. we attach a cell to each grid point.
-    return h, M, X, Y, Z
+    x, y, z = np.meshgrid(x, x, x, indexing='ij', sparse = True)	 # Evaluation cells. we attach a cell to each grid point.
+    return h, M, x, y, z
 
-#---------------------------Define J field------------------------
-def J_field(mu0,r,X,Y,Z):
+#-------------- Define J field
+def J_field(r, x, y, z):
     ''' Define analytical smooth current density field '''
            
     ## Define a mask, values outside of the mask set to be zero
-    mask=((X**2+Y**2+Z**2)<r**2) # sphere mask
+    mask=((x**2+y**2+z**2)<r**2) # sphere mask
   
     Jx=(14*y*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**2 - (8*y**3*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**3 - (4*y*z**3*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**4 - (4*y**3*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**4 - (8*y*z**3*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**3 - (8*x**2*y*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**3 - (4*x**2*y*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**4
     Jy =(14*x*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**2 - (8*x**3*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**3 - (4*x*z**3*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**4 - (4*x**3*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**4 - (8*x*z**3*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**3 - (8*x*y**2*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**3 - (4*x*y**2*z*np.exp(-1/abs((r**2-(x**2+y**2+z**2))*mask)))/(- r**2 + x**2 + y**2 + z**2)**4
@@ -63,7 +63,7 @@ def J_field(mu0,r,X,Y,Z):
     
     return mask, Jx, Jy, Jz
 
-#------------------ Circular convolution by using FFT method ------------
+#------------------ Circular convolution by using FFT method 
 def FFTCONV(h,Jx,Jy,Jz):
     ''' Calculates the magnetic field caused by a current density distribution Jx, Jy, and Jz '''
     
@@ -100,7 +100,7 @@ def FFTCONV(h,Jx,Jy,Jz):
 s = 2	                    # define the size ofcomputional domain  (-s to s)               
 r = 1	                    # radii of mask
 mu0 = 1	                    # we assumed that mu0=1
-mR = [5, 6, 7, 8, 9]        # M=2^mR[i] where M is umber of grid points in each direction.
+mR = [5, 6]        # M=2^mR[i] where M is umber of grid points in each direction.
 
 Erms = np.zeros(len(mR))    # Error vector
 TIME = np.zeros(len(mR))    # Time vector
@@ -112,7 +112,7 @@ for idx in range(0,len(mR)):
     M_2 = M/2	                # half of grid points
     M_4 = M/4	                # quarter of grid points
     
-    mask, Jx, Jy, Jz = J_field(mu0,r,x,y,z)
+    mask, Jx, Jy, Jz = J_field(r,x,y,z)
     
     time_start = time . time ()
     Bxh, Byh ,Bzh = FFTCONV(h,Jx,Jy,Jz)
